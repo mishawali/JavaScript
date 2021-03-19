dc7413ecc2b4b10aa5b835ff753ba4eee134c48e
Misha <67483963+mishawali@users.noreply.github.com>
2021-03-18 20:34:30 -0700

    Add files via upload
    assignment2

---
+++ main.js
@@ -0,0 +1,28 @@
+
+const goals = document.querySelector('.goals');
+const button = document.querySelector('.button');
+const list = document.querySelector('.list');
+
+
+
+{
+
+    const completebutton = document.createElement('button');
+
+    completebutton.innerHTML = '<i class="fas fa-check"></i>';
+
+    completebutton.classList.add('complete_button');
+
+    List.appendChild(completedbutton);
+
+ 
+
+    const deletebutton = document.createElement('button');
+
+    deletebutton.innerHTML = '<i class="fas fa-trash"></i>';
+
+    deletebutton.classList.add('delete_buton');
+
+    List.appendChild(deletebutton);
+
+}
